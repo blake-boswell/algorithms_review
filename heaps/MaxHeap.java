9438c5e6810cab5ce460d1b4beb99b48f2772baa
Blake Boswell <blake.w.boswell@gmail.com>
2018-10-28 17:39:06 -0500

    Basic MaxHeap

--- heaps/MaxHeap.java
+++ heaps/MaxHeap.java
@@ -97,4 +97,10 @@ public class MaxHeap<T extends Comparable<T>>{
             System.out.print(this.array[i] + " ");
         }
     }
+
+    public static void main(String[] args) {
+        Integer data[] = { 4, 2, 5, 1, 6, 10, 22, 13, 14 };
+        MaxHeap<Integer> maxHeap = new MaxHeap(data);
+        maxHeap.print();
+    }
 }
